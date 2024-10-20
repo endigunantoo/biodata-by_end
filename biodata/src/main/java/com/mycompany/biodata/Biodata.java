39c804f234c0d70543d82d63b428bbd82c5e26bf
endig <endig@fuck>
2024-10-20 22:24:28 +0700

    tugas biodata

---
+++ biodata/src/main/java/com/mycompany/biodata/Biodata.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biodata;
+
+/**
+ *
+ * @author endig
+ */
+public class Biodata {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
